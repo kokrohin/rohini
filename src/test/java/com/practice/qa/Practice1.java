2b8101061df0998a5b9e5f52b15b42230130e87d
kokrohin <113503065+kokrohin@users.noreply.github.com>
2022-09-19 19:37:48 +0530

    google has been automated

--- src/test/java/com/practice/qa/Practice1.java
+++ src/test/java/com/practice/qa/Practice1.java
@@ -1,11 +1,21 @@
 package com.practice.qa;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Practice1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+
+	@Test
+	public void testCase1() {
+		WebDriverManager.chromedriver().setup();
 		
-
-	}
-
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.co.in/");
+		
+	} 
 }
