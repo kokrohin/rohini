b1c2ab482193e1eb63c85e6a42b4a6671174bd31
kokrohin <113503065+kokrohin@users.noreply.github.com>
2022-09-20 10:51:54 +0530

    myntra

--- src/test/java/com/practice/qa/Practice1.java
+++ src/test/java/com/practice/qa/Practice1.java
@@ -18,12 +18,11 @@ public class Practice1 {
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.google.co.in/");
 		driver.navigate().to("https://www.amazon.in/");
-<<<<<<< HEAD
+
 		driver.navigate().to("https://www.flipkart.com/");
 		
-=======
+
 		driver.navigate().to("https://www.myntra.com/");
->>>>>>> 695fa28f996564904e99ad47c0a787285584219c
 		
 	} 
 }
